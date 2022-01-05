018186a3722282cc8fb8329f83f1e1118117288a
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-05 10:43:11 +0300

    Added list of quotes: array

---
+++ src/app/listOfQuotes.ts
@@ -0,0 +1,59 @@
+ import { Quot } from 'src/app/classes/quote';
+
+
+export let Quotes: Quot[] =[
+    new Quot(1,"In software, we rarely have meaningful requirements. Even if we do, the only measure of success that matters is whether our solution solves the customer’s shifting idea of what their problem is.",'C.A.R Hoaee',0,0,new Date( 2021,11,20),"Mishael"),
+    new Quot(2,"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.","Bjarne Stroustrup",0,0, new Date( 2021,11,25),"Rebecca"),
+    new Quot(3,"The most amazing achievement of the computer software industry is its continuing cancellation of the steady and staggering gains made by the computer hardware industry.","Henry Petroski",0,0,new Date( 2021,11,28), "Thaddius" )
+
+]
+
+// {
+//    quoteId: 0,
+//    quote:
+//    quoteAuthor: 
+//    upvotes: 0,
+//    downvotes:0,
+//    createdAt: 
+//    submittedBy:
+
+
+// },
+// {
+//    quoteId:1,
+//    quote: 
+//    quoteAuthor:
+//    upvotes: 0,
+//    downvotes:0,
+//    createdAt: 
+//    submittedBy:"Becky"
+// },
+// {
+//    quoteId:2,
+//    quote: 
+//    quoteAuthor:
+//    upvotes: 0,
+//    downvotes:0,
+//    createdAt: 
+//    submittedBy:"Thaddius"
+// },
+// {
+//     quoteId:3,
+//     quote: "Beware of bugs in the above code; I have only proved it correct, not tried it. ",
+//     quoteAuthor:"Donald Knuth",
+//     createdAt: new Date( 2021,10,15),
+// },
+// {
+//     quoteId:4,
+//     quote:"Writing the first 90 percent of a computer program takes 90 percent of the time. The remaining ten percent also takes 90 percent of the time and the final touches also take 90 percent of the time.",
+//     quoteAuthor:"N.J. Rubenking",
+//     createdAt: new Date( 2021,11,18),
+// },
+// {
+//     quoteId:5,
+//     quote:"Walking on water and developing software from a specification are easy if both are frozen.",
+//     quoteAuthor:"Edward V Berard",
+//     createdAt: new Date( 2022,11,15),
+// }
+
+// ]
